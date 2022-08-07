d009b87189ef2d566c043e24bdeb2fa9310e378b
Maksym Matlo <maksym.matlo@gmail.com>
2022-08-07 23:17:21 +0300

    Added StudentResource

---
+++ class-book-impl/src/main/java/com/eschool/classbook/student/StudentResource.java
@@ -0,0 +1,65 @@
+package com.eschool.classbook.student;
+
+import com.eschool.openapi.v1.api.StudentsV1Api;
+import com.eschool.openapi.v1.model.CommonResponseDto;
+import com.eschool.openapi.v1.model.PageViewDto;
+import com.eschool.openapi.v1.model.StudentDto;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@AllArgsConstructor
+public class StudentResource implements StudentsV1Api {
+    private static final String STUDENT_SAVED_RESPONSE_TEXT = "Student with id - %d saved successfully";
+    private static final String STUDENT_UPDATED_RESPONSE_TEXT = "Student with id - %d updated successfully";
+    private static final String STUDENT_DELETED_RESPONSE_TEXT = "Student with id - %d deleted successfully";
+    private final StudentMapper studentMapper;
+    private final StudentService studentService;
+
+    @Override
+    public ResponseEntity<StudentDto> getStudentById(Long id) {
+        StudentEntity studentEntity = studentService.findById(id);
+        StudentDto studentDto = studentMapper.toDTO(studentEntity);
+        return ResponseEntity.ok(studentDto);
+    }
+
+    @Override
+    public ResponseEntity<PageViewDto<StudentDto>> getStudentList(Pageable pageable) {
+        Page<StudentEntity> studentEntities = studentService.findAll(pageable);
+        List<StudentDto> collect = studentEntities.getContent().stream().map(studentMapper::toDTO)
+                .collect(Collectors.toList());
+        PageViewDto<StudentDto> pageViewDto = new PageViewDto<>();
+        pageViewDto.setData(collect);
+        return ResponseEntity.ok(pageViewDto);
+    }
+
+    @Override
+    public ResponseEntity<CommonResponseDto> postStudent(StudentDto studentDto) {
+        StudentEntity studentEntity = studentMapper.toEntity(studentDto);
+        StudentEntity savedStudent = studentService.save(studentEntity);
+        Long id = savedStudent.getId();
+        CommonResponseDto commonResponseDto = new CommonResponseDto(id, String.format(STUDENT_SAVED_RESPONSE_TEXT, id));
+        return ResponseEntity.ok(commonResponseDto);
+    }
+
+    @Override
+    public ResponseEntity<CommonResponseDto> updateStudent(Long id, StudentDto studentDto) {
+        StudentEntity studentEntity = studentMapper.toEntity(studentDto);
+        studentService.update(id, studentEntity);
+        CommonResponseDto commonResponseDto = new CommonResponseDto(id, String.format(STUDENT_UPDATED_RESPONSE_TEXT, id));
+        return ResponseEntity.ok(commonResponseDto);
+    }
+
+    @Override
+    public ResponseEntity<CommonResponseDto> deleteStudent(Long id) {
+        studentService.deleteById(id);
+        CommonResponseDto commonResponseDto = new CommonResponseDto(id, String.format(STUDENT_DELETED_RESPONSE_TEXT, id));
+        return ResponseEntity.ok(commonResponseDto);
+    }
+}
