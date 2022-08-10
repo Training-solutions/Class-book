de9fdb841d46f03dda759d89ed575d7337fed7c7
Maksym Matlo <maksym.matlo@gmail.com>
2022-08-10 14:45:41 +0300

    Added some changes for services

--- class-book-impl/src/main/java/com/eschool/classbook/group/GroupService.java
+++ class-book-impl/src/main/java/com/eschool/classbook/group/GroupService.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface GroupService {
-
     Page<GroupEntity> findAll(Pageable pageable);
     GroupEntity findById(Long id);
     void deleteById(Long id);
