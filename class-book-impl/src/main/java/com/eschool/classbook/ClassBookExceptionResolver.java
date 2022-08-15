95876064f37c9bf8aaa2e1142f8412598685d9c1
Maksym Matlo <maksym.matlo@gmail.com>
2022-08-15 18:11:21 +0300

    Added common exception resolver

---
+++ class-book-impl/src/main/java/com/eschool/classbook/ClassBookExceptionResolver.java
@@ -0,0 +1,22 @@
+package com.eschool.classbook;
+
+import com.eschool.classbook.exception.ClassBookException;
+import com.eschool.openapi.v1.model.ErrorView;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.OffsetDateTime;
+
+@ControllerAdvice
+public class ClassBookExceptionResolver {
+    @ExceptionHandler(ClassBookException.class)
+    public ResponseEntity<ErrorView> handleException(ClassBookException e) {
+        ErrorView errorView = new ErrorView();
+        errorView.setMessage(e.getMessage());
+        errorView.setStatus(500);
+        errorView.setTimestamp(OffsetDateTime.now());
+        return new ResponseEntity<>(errorView, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
