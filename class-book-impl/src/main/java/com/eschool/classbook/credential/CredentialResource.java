cd70a5dfa3acf20ed0819c2b25bbda3ea03d1506
Maksym Matlo <maksym.matlo@gmail.com>
2022-11-18 23:29:18 +0200

    Fixed Controllers

--- class-book-impl/src/main/java/com/eschool/classbook/credential/CredentialResource.java
+++ class-book-impl/src/main/java/com/eschool/classbook/credential/CredentialResource.java
@@ -5,45 +5,60 @@ import com.eschool.openapi.v1.model.CommonResponseDto;
 import com.eschool.openapi.v1.model.CredentialDto;
 import com.eschool.openapi.v1.model.PageViewDto;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @AllArgsConstructor
 public class CredentialResource implements CredentialsV1Api {
-
-    private static final String CREDENTIAL_SAVED_RESPONSE_TEXT = "Credential with id - %d saved successfully";
-    private static final String CREDENTIAL_UPDATED_RESPONSE_TEXT = "Credential with id - %d updated successfully";
-    private static final String CREDENTIAL_DELETED_RESPONSE_TEXT = "Credential with id - %d deleted successfully";
-
     private final CredentialMapper credentialMapper;
     private  final CredentialService credentialService;
 
-
     @Override
     public ResponseEntity<CommonResponseDto> deleteCredential(Long credentialId) {
-
-        return null;
+        credentialService.deleteById(credentialId);
+        return ResponseEntity.ok(
+            new CommonResponseDto(credentialId,
+                String.format("Credential with id - %d deleted successfully", credentialId)));
     }
 
     @Override
     public ResponseEntity<CredentialDto> getCredentialById(Long credentialId) {
-        return null;
+        CredentialEntity credentialEntity = credentialService.findById(credentialId);
+        CredentialDto credentialDto = credentialMapper.toDto(credentialEntity);
+        return ResponseEntity.ok(credentialDto);
     }
 
     @Override
     public ResponseEntity<PageViewDto<CredentialDto>> getCredentialList(Pageable pageable) {
-        return null;
+        Page<CredentialEntity> credentials = credentialService.findAll(pageable);
+        List<CredentialDto> credentialDtos = credentials.get().map(credentialMapper::toDto).collect(Collectors.toList());
+        return ResponseEntity.ok(new PageViewDto<>(credentialDtos));
     }
 
     @Override
     public ResponseEntity<CommonResponseDto> postCredential(CredentialDto credentialDto) {
-        return null;
+        CredentialEntity credentialEntity = credentialMapper.toEntity(credentialDto);
+        CredentialEntity savedCredentialEntity = credentialService.save(credentialEntity);
+        Long credentialId = savedCredentialEntity.getId();
+        return ResponseEntity.ok(
+            new CommonResponseDto(savedCredentialEntity.getId(),
+                String.format("Credential with id - %d saved successfully",credentialId)));
     }
 
     @Override
     public ResponseEntity<CommonResponseDto> updateCredential(Long credentialId, CredentialDto credentialDto) {
-        return null;
+        CredentialEntity credentialEntity = credentialMapper.toEntity(credentialDto);
+        CredentialEntity updatedCredentialEntity = credentialService.update(credentialId, credentialEntity);
+        Long savedCredentialId = updatedCredentialEntity.getId();
+        return ResponseEntity.ok(
+            new CommonResponseDto(savedCredentialId,
+                String.format("Credential with id - %d updated successfully", savedCredentialId))
+        );
     }
 }
--- class-book-impl/src/main/java/com/eschool/classbook/subject/SubjectResource.java
+++ class-book-impl/src/main/java/com/eschool/classbook/subject/SubjectResource.java
@@ -1,7 +1,5 @@
 package com.eschool.classbook.subject;
 
-import com.eschool.classbook.group.GroupEntity;
-import com.eschool.classbook.group.GroupService;
 import com.eschool.openapi.v1.api.SubjectsV1Api;
 import com.eschool.openapi.v1.model.CommonResponseDto;
 import com.eschool.openapi.v1.model.PageViewDto;
@@ -18,18 +16,13 @@ import java.util.stream.Collectors;
 @RestController
 @AllArgsConstructor
 public class SubjectResource implements SubjectsV1Api {
-
-    private static final String SUBJECT_SAVED_RESPONSE_TEXT = "Subject with id - %d saved successfully";
-    private static final String SUBJECT_UPDATE_RESPONSE_TEXT = "Subject with id - %d updated successfully";
-    private static final String SUBJECT_DELETED_RESPONSE_TEXT = "Subject with id - %d deleted successfully";
-
     private final SubjectMapper subjectMapper;
     private final SubjectService subjectService;
 
     @Override
     public ResponseEntity<CommonResponseDto> deleteSubject(Long subjectId) {
         subjectService.deleteById(subjectId);
-        CommonResponseDto commonResponseDto = new CommonResponseDto(subjectId, String.format(SUBJECT_DELETED_RESPONSE_TEXT, subjectId));
+        CommonResponseDto commonResponseDto = new CommonResponseDto(subjectId, String.format("Subject with id - %d deleted successfully", subjectId));
         return ResponseEntity.ok(commonResponseDto);
     }
 
@@ -53,7 +46,7 @@ public class SubjectResource implements SubjectsV1Api {
         SubjectEntity subjectEntity = subjectMapper.toEntity(subjectDto);
         SubjectEntity savedSubject = subjectService.save(subjectEntity);
         Long id = savedSubject.getId();
-        CommonResponseDto commonResponseDto = new CommonResponseDto(id, String.format(SUBJECT_SAVED_RESPONSE_TEXT, id));
+        CommonResponseDto commonResponseDto = new CommonResponseDto(id, String.format("Subject with id - %d saved successfully", id));
         return ResponseEntity.ok(commonResponseDto);
     }
 
@@ -61,7 +54,7 @@ public class SubjectResource implements SubjectsV1Api {
     public ResponseEntity<CommonResponseDto> updateSubject(Long subjectId, SubjectDto subjectDto) {
         SubjectEntity subjectEntity = subjectMapper.toEntity(subjectDto);
         subjectService.update(subjectId,subjectEntity);
-        CommonResponseDto commonResponseDto = new CommonResponseDto(subjectId, String.format(SUBJECT_UPDATE_RESPONSE_TEXT, subjectId));
+        CommonResponseDto commonResponseDto = new CommonResponseDto(subjectId, String.format("Subject with id - %d updated successfully", subjectId));
         return ResponseEntity.ok(commonResponseDto);
     }
 }
