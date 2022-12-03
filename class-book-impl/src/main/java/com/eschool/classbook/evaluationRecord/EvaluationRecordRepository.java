305626b58270d005f5c2b7ba134aacfd08143954
Maksym Matlo <maksym.matlo@gmail.com>
2022-12-03 11:20:54 +0200

    Rewrited evaluationRecord logic

--- class-book-impl/src/main/java/com/eschool/classbook/evaluationRecord/EvaluationRecordRepository.java
+++ class-book-impl/src/main/java/com/eschool/classbook/evaluationRecord/EvaluationRecordRepository.java
@@ -1,12 +1,13 @@
 package com.eschool.classbook.evaluationRecord;
 
+import com.eschool.classbook.group.GroupEntity;
 import com.eschool.openapi.v1.model.EvaluationRecordDto;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface EvaluationRecordRepository {
-    List<EvaluationRecordDto> getEvaluationRecords(Long subjectId, Long teacherId,
-                                                   Long groupId, Long studentId,
-                                                   LocalDateTime startDate, LocalDateTime endDate);
+    List<GroupEntity> getEvaluationRecords(Long subjectId, Long teacherId,
+                                           Long groupId, Long studentId,
+                                           LocalDateTime startDate, LocalDateTime endDate);
 }
